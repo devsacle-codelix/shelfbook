e9d11016582a950ed95b95b19ea3488b2b254962
Rajib Rindang <rajibrindang29@gmail.com>
2026-02-15 02:30:50 +0700

    init monorepo and CICD

--- apps/platform/src/routes/index.tsx
+++ apps/platform/src/routes/index.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@shelfbook/ui";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return <div>Platform</div>;
+	console.log(import.meta.env.VITE_TEST);
+	return (
+		<div>
+			<Button label="Click Me" />
+		</div>
+	);
 }
